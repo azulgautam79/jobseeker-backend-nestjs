f9babeccff0b223402fe23cde18fd33c453cddc2
Azul Gautam <azulgautam79@gmail.com>
2026-07-24 21:24:56 +0545

    [Otel configuration started]

--- src/modules/test/test.module.ts
+++ src/modules/test/test.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TestService } from './test.service';
 import { TestController } from './test.controller';
+import { LoggerService } from '../../common/logger/logger.service';
 
 @Module({
   controllers: [TestController],
-  providers: [TestService],
+  providers: [TestService, LoggerService],
 })
 export class TestModule {}
